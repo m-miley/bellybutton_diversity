f8381b87b19084d86b99c1639c006c60eb290de6
markm3690 <mrmileyy@gmail.com>
2022-06-09 23:07:35 -0500

    gauge chart

--- charts.js
+++ charts.js
@@ -63,9 +63,54 @@ function buildCharts(sample) {
       
       // 4. Create a variable that filters the samples for the object with the desired sample number.
       let filteredSamples = samples.filter(obj => obj.id == sample);
-
+      
       // 3.1. Create a variable that filters the metadata array for the object with the desired sample number.
+      let metadata = data.metadata;
+
+      // 3.2. Create a variable that holds the first sample in the metadata array.
+      let theMetaData = metadata.filter(obj => obj.id == sample)[0];
+      console.log(theMetaData);
+
+      // 3.3. Create a variable that holds the washing frequency.
+      let wfreq = theMetaData.wfreq
+      
     
+
+      /////////////// GAUGE CHART ///////////////
+      // 4. Create the trace for the gauge chart.
+      var gaugeData = [{
+        domain: { x: [0, 1], y: [0, 1] },
+        value: wfreq,
+        title: { text: '<b>Belly Button Washing Frequency</b><br>Scrubs per Week' },
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+          bar: { color: "dodgerblue" },
+          axis: { 
+            range: [null, 10],
+            tickwidth: 2,
+            tickcolor: 'black'
+          },
+          steps: [
+            { range: [0, 2], color: "orangered" },
+            { range: [2, 4], color: "yellowgreen" },
+            { range: [4, 6], color: "green" },
+            { range: [6, 8], color: "yellowgreen" },
+            { range: [8, 10], color: "gold" }
+          ]
+          
+        }
+      }];
+
+      // 5. Create the layout for the gauge chart.
+      var gaugeLayout = { 
+        
+        automargin: true
+      };
+
+      // 6. Use Plotly to plot the gauge data and layout.
+      Plotly.newPlot("gauge", gaugeData);
+
       //  5. Create a variable that holds the first sample in the array.
       let theSample = filteredSamples[0];
       
@@ -94,12 +139,12 @@ function buildCharts(sample) {
 
   // //   9. Create the layout for the bar chart. 
       var barLayout = {
-          title: "Top 10 Bacteria Cultures Identified",
+          title: "<b>Top 10 Bacteria Cultures Identified</b>",
           margin: {
             l: 125,
             r: 125,
-            b: 150,
-            t: 25
+            b: 100,
+            t: 40
           }
       };
     // 10. Use Plotly to plot the data with the layout. 
@@ -126,7 +171,7 @@ function buildCharts(sample) {
 
     // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
-      title: 'Bacteria Cultures Per Sample',
+      title: '<b>Bacteria Cultures Per Sample</b>',
       xaxis: { title: 'OTU ID'},
       margin: {
         l: 90,
